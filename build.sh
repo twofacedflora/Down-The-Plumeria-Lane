20fd9248fb2aa773b7d3cc921c9b220e85d450be
twofacedflora <tkakari@proton.me>
2023-05-25 00:39:04 +0800

    updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-rm -rv output/.data
-mkdir -v output/.data
-cp -v resources/data/settings.json output/.data
-rm -v bin/downtheplumerialane/*.class
-javac -verbose -cp .:lib/json.jar:lib/gson-2.10.1.jar -d bin src/downtheplumerialane/*.java
-jar -cvfm output/DTPL.jar MANIFEST.mf lib resources -C bin .
+# create output directory
+mkdir output
+# create data directory
+mkdir output/.data
+# add settings file to data directory
+cp resources/data/settings.json output/.data
+# build class files to bin directory
+javac -cp .:lib/json.jar:lib/gson-2.10.1.jar -d bin src/downtheplumerialane/*.java
+# build JAR archive to output directory
+jar -cfm output/DTPL.jar MANIFEST.mf lib resources -C bin .
