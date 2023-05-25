004fc05583f89e570e06ea05f22e878e38442e96
twofacedflora <tkakari@proton.me>
2023-05-25 21:40:10 +0800

    removed org.json library

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ mkdir output/.data
 # add settings file to data directory
 cp resources/data/settings.json output/.data
 # build class files to bin directory
-javac -cp .:lib/json.jar:lib/gson-2.10.1.jar -d bin src/downtheplumerialane/*.java
+javac -cp .:lib/gson-2.10.1.jar -d bin src/downtheplumerialane/*.java
 # build JAR archive to output directory
 jar -cfm output/DTPL.jar MANIFEST.mf lib resources -C bin .
